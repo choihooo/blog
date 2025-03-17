479e58c45dc0b734dfc376e9427f877c99ccaf65
choihooo <hochoi8621@naver.com>
2025-03-17 16:12:21 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { RenderRouter } from "./routes";
+
+function App() {
+  return (
+    <>
+      <RenderRouter />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <div>hasdsad</div>;
+};
+
+export default HomePage;
